446495c212773b69caab619c5ee10bc6fee64804
Пользователь <777draft777@mail.ru>
2026-06-30 12:18:24 +0300

    Задание 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -20,15 +20,42 @@ public class Main {
             int remainingDistance = allDistance - coveredDistance;
             System.out.println("Держитесь! Осталось " + remainingDistance + " метров");
             coveredDistance += 500;
-        }
-        while (coveredDistance <= 42195);
+        } while (coveredDistance <= 42195);
 
         System.out.println("С циклом for");
         int d = 0;
         System.out.println("Пройденная дистанция");
-        for (; d <= 42195; d+=500) {
+        for (; d <= 42195; d += 500) {
             int remainingDistance = allDistance - d;
             System.out.println("Держитесь! Осталось " + remainingDistance + " метров");
         }
+
+        System.out.println("Задание 3");
+        int remainingFunds = 2800;
+        int parkingDays = 0;
+        while (remainingFunds >= 100) {
+            parkingDays++;
+            if (parkingDays % 5 == 0) {
+                System.out.println(parkingDays + "-й день — бесплатно!");
+                continue;
+            }
+            remainingFunds -= 100;
+        }
+        System.out.println("Бюджет позволяет стоять " + parkingDays + " дней.");
+
+        System.out.println("С циклом for");
+        int budget = 2800;
+        int paidDays = 0;
+        for (; budget > 0; ) {
+            paidDays++;
+            if (paidDays % 5 == 0) {
+                System.out.println(paidDays + "-й день — бесплатно!");
+                continue;
+            }
+            budget -= 100;
+        }
+        System.out.println("Бюджет позволяет стоять " + paidDays + " дней.");
     }
 }
+
+
